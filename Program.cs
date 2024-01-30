8c1e5becee5c24da2fed186c5879194dab99579c
Charles Zhang <szinubuntu@gmail.com>
2024-01-30 16:56:52 -0500

    Enable CORS policy

--- Program.cs
+++ Program.cs
@@ -2,13 +2,26 @@ namespace StaticWebpagesServer
 {
     public class Program
     {
+        private static readonly string _policyName = "CorsPolicy";
+
         public static void Main(string[] args)
         {
-            WebApplication app = WebApplication.CreateBuilder(new WebApplicationOptions()
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
             {
                 WebRootPath = args.First()
-            }).Build();
+            });
+            builder.Services.AddCors(opt =>
+            {
+                opt.AddPolicy(name: _policyName, builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+            WebApplication app = builder.Build();
 
+            app.UseCors(_policyName);
             app.UseStaticFiles();
             app.MapGet("/", () => "This is a static file server!");
 
