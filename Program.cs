bdf72b55bce3f2f2a6984de378e05387ae4fd579
Charles Zhang <szinubuntu@gmail.com>
2024-01-30 17:06:15 -0500

    Add necessary headers for Godot4 web packages

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@ namespace StaticWebpagesServer
                 WebRootPath = args.First()
             }).Build();
 
+            app.Use(async (context, next) =>
+            {
+                // Per: https://github.com/godotengine/godot-proposals/issues/6616
+                context.Response.Headers.Append("Cross-Origin-Opener-Policy", "same-origin");
+                context.Response.Headers.Append("Cross-Origin-Embedder-Policy", "require-corp");
+                await next.Invoke();
+            });
             app.UseStaticFiles();
             app.MapGet("/", () => "This is a static file server!");
 
